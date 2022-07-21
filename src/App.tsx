7409b69024d2c022ab97f0b321f9e8247e5dba7b
Anaru Herbert <anaruherbert@gmail.com>
2022-07-21 10:47:33 +1000

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import Home from "components/Home";
+import { AppBar} from '@mui/material';
+import { createStyles, makeStyles } from '@mui/styles';
 
-const App: React.FC = () => {    
+const useStyles = makeStyles(() =>
+  createStyles({
+    appBar: {
+      display: 'flex',
+      minHeight: '7vh',      
+      background: '#5b616b',      
+      boxShadow: 'none',     
+    }    
+  })
+);
+
+const App: React.FC = () => { 
+    const classes = useStyles();      
 
   return (
+
     <div className='app'>
+      <AppBar position="fixed" className={classes.appBar}>
+        
+      </AppBar>
       <Home></Home>  
     </div>
     
