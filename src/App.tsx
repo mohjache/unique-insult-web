656d0ddbe0590ec5f90198120061cd5c1502c849
Anaru Herbert <anaruherbert@gmail.com>
2022-07-21 13:24:37 +1000

    Styled app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import Home from "components/Home";
-import { AppBar} from '@mui/material';
-import { createStyles, makeStyles } from '@mui/styles';
+import Home from 'components/Home'
+import {AppBar} from '@mui/material'
+import {createStyles, makeStyles} from '@mui/styles'
 
 const useStyles = makeStyles(() =>
   createStyles({
     appBar: {
       display: 'flex',
-      minHeight: '7vh',      
-      background: '#5b616b',      
-      boxShadow: 'none',     
-    }    
-  })
-);
+      minHeight: '7vh',
+    },
+  }),
+)
 
-const App: React.FC = () => { 
-    const classes = useStyles();      
+const App: React.FC = () => {
+  const classes = useStyles()
 
   return (
-
-    <div className='app'>
-      <AppBar position="fixed" className={classes.appBar}>
-        
-      </AppBar>
-      <Home></Home>  
+    <div className="app">
+      <AppBar
+        aria-label="menu"
+        sx={{bgcolor: '#5b616b', boxShadow: 'none'}}
+        position="fixed"
+        className={classes.appBar}></AppBar>
+      <Home></Home>
     </div>
-    
-  );
+  )
 }
 
-export default App;
+export default App
