906a137554326a9d551acfd0ef9703d2094fc79d
Anaru Herbert <anaruherbert@gmail.com>
2022-07-21 13:24:30 +1000

    Added prettier config

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { createStyles, makeStyles } from '@mui/styles';
+import React from 'react'
+import {createStyles, makeStyles} from '@mui/styles'
 
 const useStyles = makeStyles(() =>
   createStyles({
     container: {
       display: 'flex',
       justifyContent: 'center',
-      alignItems: 'center', 
-      minHeight: '75vh'           
-    }    
-  })
-);
+      alignItems: 'center',
+      minHeight: '75vh',
+    },
+  }),
+)
 
-const Home: React.FC = () => {    
-    const classes = useStyles();
-    return (
-        <div className={classes.container}>
-            <h1>Welcome to Poo Puffin</h1>
-        </div>
-    )
-} 
+const Home: React.FC = () => {
+  const classes = useStyles()
+  return (
+    <div className={classes.container}>
+      <h1>Welcome to Poo Puffin</h1>
+    </div>
+  )
+}
 
-export default Home;
+export default Home
